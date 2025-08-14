9b9e0820878171b0990ca2541530e2e5b57265b8
John Brzustowski <john.brzustowski@gmail.com>
2025-08-14 18:29:48 -0300

    drop obsolete stuff now in fleetsie

--- umount_image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo umount ./image_root
-sudo umount ./image_boot
-sudo kpartx -d ./motus-audio-*.img
--- update_compressed_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./umount_image.sh
-img=`ls -1 motus-audio-*.img`
-cat $img | xz -zc > ${img}.xz
